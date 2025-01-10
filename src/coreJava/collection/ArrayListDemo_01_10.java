c4e6a123b68e0fcf417be2336d4fd7809469686a
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-01-10 17:24:01 +0530

    ArrayListDemo_01_10

---
+++ src/coreJava/collection/ArrayListDemo_01_10.java
@@ -0,0 +1,49 @@
+package coreJava.collection;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo_01_10 {
+
+	public static void main(String[] args) {
+		
+		ArrayList a= new ArrayList();
+		
+		
+		a.add("A");
+		
+		a.add(10);
+		
+		a.add("M");
+		
+		a.add(null);
+		
+		
+		a.add(11);
+		
+		
+		
+		System.out.println(a);
+		
+		
+		a.remove(2);
+		
+		System.out.println(a);
+		
+		
+		a.add(2, "AMIT");
+		
+		System.out.println(a);
+		
+		a.add(21);
+		
+		
+		System.out.println(a);
+	
+		
+		
+		
+		
+		
+	}
+
+}
