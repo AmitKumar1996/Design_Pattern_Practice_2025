4b639a2f4285cf5946a1c0ba82fb9e67b239d4dd
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-01-11 14:21:25 +0530

    LinkedListDemo

--- src/coreJava/collection/LinkedList/LinkedListDemo.java
+++ src/coreJava/collection/LinkedList/LinkedListDemo.java
@@ -1,10 +1,25 @@
 package coreJava.collection.LinkedList;
 
+import java.util.LinkedList;
+
 public class LinkedListDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		LinkedList l= new LinkedList();
+		
+		l.add("Amit");
+		
+		l.add("kumar");
+		
+		l.add("yadav");
+		
+		
+	
+		System.out.println(l
+				);
 	}
 
 }
