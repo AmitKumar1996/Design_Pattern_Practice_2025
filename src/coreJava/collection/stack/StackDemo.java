c2a013690c5e06dbf690e7dfd091858b93f9116e
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-01-11 14:48:46 +0530

    StackDemo

---
+++ src/coreJava/collection/stack/StackDemo.java
@@ -0,0 +1,30 @@
+package coreJava.collection.stack;
+
+import java.util.Stack;
+
+public class StackDemo {
+
+	public static void main(String[] args) {
+	
+		Stack s= new Stack();
+		
+		
+		s.add('r');
+		s.add('a');
+		s.add('m');
+		s.add('u');
+		s.add('k');
+		
+		System.out.println(s);
+		
+		s.add('t');
+		s.add('i');
+		
+		s.add('m');
+		s.add('A');
+		
+		System.out.println(s.pop());
+
+	}
+
+}
