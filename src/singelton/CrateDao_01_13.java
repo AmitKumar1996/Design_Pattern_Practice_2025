c4440c1a9ad866550dffda2878e962562f87989e
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-01-13 12:23:34 +0530

    CrateDao_01_13

---
+++ src/singelton/CrateDao_01_13.java
@@ -0,0 +1,32 @@
+package singelton;
+
+public class CrateDao_01_13 {
+	
+	private static CrateDao_01_13 crateDao_01_13=null;
+	
+	private static CrateDao_01_13 getInstance() {
+		
+		if(crateDao_01_13 == null) {
+		  crateDao_01_13= new  CrateDao_01_13();
+			
+		}
+		return crateDao_01_13;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		
+		CrateDao_01_13 obj1= new CrateDao_01_13().getInstance();
+		System.out.println(obj1.hashCode());
+		
+		
+		CrateDao_01_13 obj2 = new CrateDao_01_13().getInstance();
+		System.out.println(obj2.hashCode());
+		
+		
+	
+
+	}
+
+}
