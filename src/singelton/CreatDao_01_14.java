ea25ac8225ca97f963d99749e10a5d036f1fc97b
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-01-14 21:48:26 +0530

    CreatDao_01_14

---
+++ src/singelton/CreatDao_01_14.java
@@ -0,0 +1,26 @@
+package singelton;
+
+public class CreatDao_01_14 {
+	
+	
+	private static  CreatDao_01_14 ceCreatDao_01_14=null;
+	
+	private CreatDao_01_14 getInstance() {
+		if(ceCreatDao_01_14 == null) {
+			ceCreatDao_01_14 = new CreatDao_01_14();
+		}
+		return ceCreatDao_01_14;
+	}
+
+	public static void main(String[] args) {
+		
+		CreatDao_01_14 obj1= new CreatDao_01_14().getInstance();
+		System.out.println(obj1.hashCode());
+		
+		CreatDao_01_14 obj2= new CreatDao_01_14().getInstance();
+		
+		System.out.println(obj2.hashCode());
+
+	}
+
+}
