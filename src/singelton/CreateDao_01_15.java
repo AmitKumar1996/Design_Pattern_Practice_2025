6fb2f60834e963289015aa2a1ca9e5b12029af36
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-01-15 15:40:54 +0530

    CreateDao_01_15

---
+++ src/singelton/CreateDao_01_15.java
@@ -0,0 +1,32 @@
+package singelton;
+
+public class CreateDao_01_15 {
+	
+	private static  CreateDao_01_15 createDao_01_15=null;
+	
+	private static CreateDao_01_15 getInstance() {
+		
+		if(createDao_01_15 == null) 
+		{
+			createDao_01_15 = new CreateDao_01_15();
+			
+		}
+		return createDao_01_15;
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+		CreateDao_01_15 obj1= new CreateDao_01_15().getInstance();
+		System.out.println(obj1.hashCode());
+		
+		CreateDao_01_15 obj2= new CreateDao_01_15().getInstance();
+		
+		System.out.println(obj2.hashCode());
+		// TODO Auto-generated method stub
+
+	}
+
+}
