53c433be7f8192f531e4dff655158fa4e217664a
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-01-24 00:03:20 +0530

    static import

---
+++ src/declarationAndAccessModifier/staticImport.java
@@ -0,0 +1,17 @@
+package declarationAndAccessModifier;
+
+import static java.lang.Math.sqrt;
+
+import static java.lang.Math.*;
+
+public class staticImport {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println(sqrt(4));
+		System.out.println(max(10, 20));
+		System.out.println(random());
+	}
+
+}
