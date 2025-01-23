5f175d885d9be5f23beaf875b8c432e50eaa8bc1
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-01-23 21:52:15 +0530

    Amit.java

---
+++ src/declarationAndAccessModifier/Amit.java
@@ -0,0 +1,19 @@
+package declarationAndAccessModifier;
+
+class Amit {
+ public static void main(String[] args) {
+	 System.out.println("Hi Amit  class this 0 ");
+ }
+}
+
+class Amit2 {
+	 public static void main(String[] args) {
+		 System.out.println("Hi Amit  class this 1 ");
+	 }
+	}
+
+class Amit3 {
+	 public static void main(String[] args) {
+		 System.out.println("Hi Amit  class this 2 ");
+	 }
+	}
