3dfa9fb3845ead7e095726e0ddf5f66f4f981927
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-01-24 00:53:57 +0530

    fully Qualified Name

---
+++ src/declarationAndAccessModifier/fullyQualifiedName.java
@@ -0,0 +1,16 @@
+package declarationAndAccessModifier;
+
+import static java.lang.System.out;
+
+public class fullyQualifiedName{
+
+	public static void main(String[] args) {
+		
+		
+		out.print("Amit ");
+		out.print("kumar");
+		// TODO Auto-generated method stub
+
+	}
+
+}
