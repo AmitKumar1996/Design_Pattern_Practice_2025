241949a989bf7f35f90d57df60a0dbb13ee0e762
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-02-13 13:26:56 +0530

    ReverseArray

---
+++ src/Java_8/ReverseArray.java
@@ -0,0 +1,48 @@
+package Java_8;
+
+import java.util.HashMap;
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+		
+		
+//		int[] array= {1,2,3,4,5,6,7,8,9};
+//		
+//		// 9 8 7 6 5 4 3 2 1
+//		
+//		int start=0;
+//		
+//		int end= array.length-1;
+//		System.out.println(array);
+//		
+//		while(start<end) {
+//			
+//			int temp=array[start];
+//			array[start]=array[end];
+//			array[end]=temp;
+//			start++;
+//			end--;
+//		}
+//		
+//		for(int i=0; i<array.length;i++)
+//		System.out.print(" "+array[i]);
+		
+		
+		String s="aabbccd";
+		
+		HashMap<String, Integer> map=new HashMap<>();
+		
+		for(int i=0;i<s.length();i++) {
+			if(map.containsKey(s.charAt(i))) {
+				map.put(s.charAt(i), )
+			}
+			map.put()
+		}
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
