dc4d96c288a997be01dcc8eebda71917eba11b17
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-02-14 19:10:05 +0530

    SumOfOddNumbers

---
+++ src/Java_8/SumOfOddNumbers.java
@@ -0,0 +1,14 @@
+package Java_8;
+import java.util.Arrays;
+
+public class SumOfOddNumbers {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        int sumOfOdds = Arrays.stream(numbers)
+                              .filter(n -> n % 2 != 0)  // Filters odd numbers
+                              .sum();  // Sums them up
+
+        System.out.println("Sum of odd numbers: " + sumOfOdds);
+    }
+}
