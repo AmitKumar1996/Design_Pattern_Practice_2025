6661986e4bc8b06a4710064c528877b7b888a0ed
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-02-20 23:44:02 +0530

    final class parents

---
+++ src/declarationAndAccessModifier/Final/parents.java
@@ -0,0 +1,15 @@
+package declarationAndAccessModifier.Final;
+
+final class parents {
+
+	static int x=10;
+	public static void main(String[] args) {
+		
+		
+		x=999;
+		System.out.println(x);
+		// TODO Auto-generated method stub
+
+	}
+
+}
