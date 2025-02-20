bf13be40a1712102929f72e6ccecccd60249496b
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-02-20 23:35:04 +0530

    final method can not override

---
+++ src/declarationAndAccessModifier/Final/child.java
@@ -0,0 +1,12 @@
+package declarationAndAccessModifier.Final;
+
+class child extends Parent{
+	
+//	public void marriage() {
+//		
+//		System.out.println("Thamanna");
+//		
+//	}     this we can not Override 
+	
+
+}
