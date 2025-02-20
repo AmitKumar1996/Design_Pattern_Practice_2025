acb6b8b240edb7083707b93143fc5639fda38ae8
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-02-21 00:22:27 +0530

    Test is abstract can't be instantiated

---
+++ src/declarationAndAccessModifier/Abstract/Test.java
@@ -0,0 +1,10 @@
+package declarationAndAccessModifier.Abstract;
+
+abstract class Test {
+	
+  public static void main(String[] args) {
+	  
+//	  Test t= new Test();   // Test is abstract can't be instantiated 
+  }
+
+}
