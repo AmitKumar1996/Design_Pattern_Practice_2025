f557a1d22100572c63a52c0d4436784a06aa9616
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-02-21 01:03:29 +0530

    messing method body , or decleare abstract

--- src/declarationAndAccessModifier/Abstract/Parents.java
+++ src/declarationAndAccessModifier/Abstract/Parents.java
@@ -1,5 +1,5 @@
 package declarationAndAccessModifier.Abstract;
 
  class Parents {
-  // public void methodOne();      // messing method body , or decleare abstract
+  // public void methodOne();      // messing method body , or decleare abstract  public void methodOne()
 }
