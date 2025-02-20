1ceb29d05870a23bc66fa69192adf41ff9d7eba6
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-02-20 20:40:20 +0530

    Default

--- src/declarationAndAccessModifier/Default/pack1/Test.java
+++ src/declarationAndAccessModifier/Default/pack1/Test.java
@@ -2,7 +2,7 @@ package declarationAndAccessModifier.Default.pack1;
 
 class Test {
 	
-	public void methoidOne() {
+	public void methodOne() {
 		System.out.println("This class MethodOne is Executed");
 	}
 
--- src/declarationAndAccessModifier/Default/pack2/Test1.java
+++ src/declarationAndAccessModifier/Default/pack2/Test1.java
@@ -1,5 +1,13 @@
 package declarationAndAccessModifier.Default.pack2;
 
- class Test1 {
+//import declarationAndAccessModifier.Default.pack1.Test;
+//
+//class Test1 {
+//	 
+//	 public static void main(String[] args) {
+//		 Test t= new Test();
+//		 t.methodOne();   // differant pkg me access nhi kar sakte hai 
+//	 }
+	 
 
-}
+//}
