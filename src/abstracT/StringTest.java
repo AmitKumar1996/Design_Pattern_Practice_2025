084a2e96636a5a8b58e7f0ab48f70323c09f0fb7
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-02-24 23:02:06 +0530

    Stream problem in interview

---
+++ src/abstracT/StringTest.java
@@ -0,0 +1,52 @@
+package abstracT;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import declarationAndAccessModifier.pkg.staticImport;
+
+
+public class StringTest {
+	
+
+//String sentence="Take a right for a quick exit";
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+     String sentence="Take a right for a quick exit";
+     
+     //o/p:-  g
+     
+     
+     ArrayList<String> list=new ArrayList<>();
+     
+    for(int i=0;i<sentence.length();i++) {
+     list.add(sentence.charAt(i));
+     
+    }
+    
+    System.out.println(list);
+     
+     
+     
+     
+     
+    System.out.println(list.size());
+     
+     
+     //Character any = list.stream().filter(s -> s.equals(list)).findAny();
+     
+    // System.out.println(any);
+     
+     
+     
+
+
+		
+	}
+
+}
