e0b5525b13ab5c2d1a7bdeb83c8d5731b8194aa0
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-02-28 09:21:08 +0530

    CharacterFrequency

--- src/Java_8/StreemAPI/FrequencyOfCharacter.java
+++ src/Java_8/StreemAPI/FrequencyOfCharacter.java
@@ -1,6 +1,7 @@
 package Java_8.StreemAPI;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -14,13 +15,27 @@ public class FrequencyOfCharacter {
 		
 		String s="aaababahaaha";
 		
-		List<Character> list= new ArrayList();
+		List<Character> list= new ArrayList<>();
 		
 		for(int i=0; i< s.length();i++) {
 			list.add(s.charAt(i));
 			
 		}
 		System.out.println(list);
+		
+		
+		HashMap<Character, Integer> map=new HashMap<>();
+		
+		for(Character c : list) {
+			if(map.containsKey(c)) {
+				map.put(c, map.get(c)+1);
+			}
+			else {
+				map.put(c, 1);
+			}
+		}
+		
+		System.out.println(map);
 		
 		Map<Character, Long> collect = list.stream().collect(Collectors.groupingBy(c -> c, Collectors.counting()));
 		System.out.println(collect);
