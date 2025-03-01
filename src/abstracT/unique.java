f9d1a490f8edf325b38002aaf9462973ed065038
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-03-01 17:07:02 +0530

    unique

---
+++ src/abstracT/unique.java
@@ -0,0 +1,34 @@
+package abstracT;
+
+import java.util.HashSet;
+
+public class unique {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array= {7,9,6,1,5,7,9,1,3,6,3}; // 1 3 5 6 7 9
+		
+		HashSet<Integer> set=new HashSet<Integer>();
+		
+		for(int i=0;i<array.length;i++) {
+			int temp=array[i];
+		int	counter=0;
+			for(int j=0;j<array.length;j++) {
+				
+				if(array[j]==temp ) {
+					counter++;
+					if(counter>1)
+					set.add(temp);
+				}
+				
+			}
+				
+		}
+		System.out.println(set);
+		for(Integer i: set) {
+			
+		}
+
+	}
+
+}
