a8aadc83a05d4b464ff6db6a4759f1992b22062c
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-04-09 15:44:58 +0530

    map() vs flatMap()

--- src/Java_8/StreemAPI/mapVsFlatMap/MapVsFlatMap.java
+++ src/Java_8/StreemAPI/mapVsFlatMap/MapVsFlatMap.java
@@ -1,6 +1,7 @@
 package Java_8.StreemAPI.mapVsFlatMap;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MapVsFlatMap {
 
@@ -9,6 +10,33 @@ public class MapVsFlatMap {
 		
 List<customer> customers=EkartDataBase.getAll();
 
+  
+
+
+
+//List<Customer>  convert List<String> -> Data Transformation
+//mapping : customer -> customer.getEmail()
+//customer -> customer.getEmail()  one to one mapping
+         List<String> emails=  customers.stream().map(c->c.getEmail()).collect(Collectors.toList());
+         System.out.println(emails);
+         
+       //customer -> customer.getPhoneNumbers()  ->> one to many mapping
+         //customer -> customer.getPhoneNumbers()  ->> one to many mapping   
+         
+         
+    List<List<Long>> phoneNumbers= customers.stream().map(p->p.getPhone()).collect(Collectors.toList());
+    
+    System.out.println(phoneNumbers);
+    
+    
+    
+    //List<Customer>  convert List<String> -> Data Transformation
+    //mapping : customer -> phone Numbers
+    //customer -> customer.getPhoneNumbers()  ->> one to many mapping
+    
+   List<Long> phone= customers.stream().flatMap(ph->ph.getPhone().stream()).collect(Collectors.toList());
+   
+   System.out.println(phone);
 
 
 	}
