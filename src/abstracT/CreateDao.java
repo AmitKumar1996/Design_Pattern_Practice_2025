812d706b17e482b86462f928a610f78d9b4478e9
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-04-16 23:50:28 +0530

    Singleton Thread safe

---
+++ src/abstracT/CreateDao.java
@@ -0,0 +1,48 @@
+package abstracT;
+
+public class CreateDao {
+	
+	private static CreateDao createDao=null;
+	
+	private static synchronized CreateDao getInstance() {
+	  if(createDao == null) {
+		  createDao= new CreateDao();
+	  }
+	  return createDao;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		CreateDao obj1= new CreateDao().getInstance();
+		System.out.println(obj1.hashCode());
+		CreateDao obj2= new CreateDao().getInstance();
+		System.out.println(obj2.hashCode());
+
+	}
+
+}
+
+/*
+
+4X4
+A B
+
+1  0 0  0
+0  1  1 1
+0  0  1 0
+0  0  0 1
+
+rowCheck(){
+fo
+ 
+}
+
+
+
+
+*/
+
+
