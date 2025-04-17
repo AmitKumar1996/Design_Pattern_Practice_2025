98b0c6deb70ed31f927735727af72d9cc0a3f9dd
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-04-18 00:03:14 +0530

    Example-2

---
+++ src/abstracT/example_2/Patent.java
@@ -0,0 +1,10 @@
+package abstracT.example_2;
+
+//public class Patent {
+//    public abstract void methodOne() {} // ❌ This is invalid!
+//}
+
+
+public abstract class Patent {
+    public abstract void methodOne(); // ✔️ No body for abstract method
+}
