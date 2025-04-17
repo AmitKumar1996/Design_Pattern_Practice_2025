47a5c163d428381ccba9e17c01abea10271f3be9
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-04-17 23:59:30 +0530

    example-1

---
+++ src/abstracT/Example_1/Patent.java
@@ -0,0 +1,10 @@
+package abstracT.Example_1;
+
+public abstract class Patent {
+    public abstract void methodOne(); // correct
+}
+
+
+//public class Patent {
+//    public abstract void methodOne(); // ❌ ERROR here
+//}
