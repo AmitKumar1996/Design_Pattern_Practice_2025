eda5b8ce14a04ede047604e55f4eb15b795ed0bf
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-04-28 19:48:02 +0530

    thread

---
+++ src/thread/threadDemo.java
@@ -0,0 +1,29 @@
+package thread;
+
+
+class MyThread extends Thread{
+	
+	public void run() {
+		for(int i=0;i<10;i++) {
+			System.out.println("Amit Thread"+i);
+		}
+	}
+	
+}
+
+public class threadDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MyThread t=new MyThread();
+		//t.start(); // created new thread every time 
+		t.run();  // this is normal method by main thread
+		
+		for(int i=5;i>0;i--) {
+			System.out.println("This is my Thread2:- "+i);
+		}
+
+	}
+
+}
