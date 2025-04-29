18c12bd47f2d95f3e84019f8183f0bafa6c38378
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-04-30 00:03:56 +0530

    Rotate Array

--- src/abstracT/RotateArray.java
+++ src/abstracT/RotateArray.java
@@ -2,7 +2,7 @@ package abstracT;
 
 import java.util.Arrays;
 
-public class Demo {
+public class RotateArray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6};
         int k = 2;
