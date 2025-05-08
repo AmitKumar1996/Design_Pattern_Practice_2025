b1d87cfb36d32ac8158fe6576a26f7f33ecf046b
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-05-08 23:25:41 +0530

    second highest

--- src/Java_8/StreemAPI/ConcatenateStrings.java
+++ src/Java_8/StreemAPI/ConcatenateStrings.java
@@ -1,15 +1,31 @@
 package Java_8.StreemAPI;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class ConcatenateStrings {
     public static void main(String[] args) {
-        List<String> words = Arrays.asList("Java", "Stream", "API");
-        
-        String result = words.stream().collect(Collectors.joining(" "));
-
-        System.out.println("Concatenated String: " + result);
+//        List<String> words = Arrays.asList("Java", "Stream", "API");
+//        
+//        String result = words.stream().collect(Collectors.joining(" "));
+//
+//        System.out.println("Concatenated String: " + result);
+    	
+    	List<String> asList = Arrays.asList("Java", "is", "Programing", "Language");
+    	
+    	List<Integer> asList2 = Arrays.asList(1,2,3,4,5,6,7,8,6,7,9,11);
+    	
+    	
+    	String collect = asList.stream().collect(Collectors.joining(" "));
+    	
+    	
+    	System.out.println(collect);
+    	
+    	
+    	Optional<Integer> first = asList2.stream().distinct().sorted(Comparator.reverseOrder()).skip(1).findFirst();
+    	System.out.println(first.get());
     }
 }
