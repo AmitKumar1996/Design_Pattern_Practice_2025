74f83331beaff46502f8940866842121d69abef0
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-05-18 23:30:01 +0530

    without Executer Framework

--- src/Executor/Main.java
+++ src/Executor/Main.java
@@ -4,7 +4,49 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		long startTime = System.currentTimeMillis();  // 1 January 1970
+		
+		Thread[] threads=new Thread[9];
+		for(int i=1;i<10;i++) {
+			int finalI=i;
+		threads[i-1]	=new Thread(()->{
+				long result = factorial(finalI);
+				System.out.println(result);
+			}
+					);
+			
+			threads[i-1].start();
+//			long factorial = factorial(i);
+//			System.out.println(factorial);
+		}
+		
+		
+		for(Thread thread: threads) {
+			try{
+				thread.join();
+				
+			}
+			catch(InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+		
+  System.out.println("Total time: "+ (System.currentTimeMillis()-startTime));
+	}
 
+	
+	private static long factorial(int n) {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		long result=1;
+		for(int i=1;i<=n;i++) {
+			result *=i;
+		}
+		return result;
 	}
 
 }
