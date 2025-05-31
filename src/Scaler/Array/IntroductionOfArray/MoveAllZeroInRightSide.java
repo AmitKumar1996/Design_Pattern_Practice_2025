25a564c0a708e1bbbe1f6807b85a8e2099e3439c
Amit Kumar <kumaramit1996.1@gmail.com>
2025-05-31 20:52:42 +0530

    Updated code

--- src/Scaler/Array/IntroductionOfArray/MoveAllZeroInRightSide.java
+++ src/Scaler/Array/IntroductionOfArray/MoveAllZeroInRightSide.java
@@ -1,50 +1,37 @@
 package Scaler.Array.IntroductionOfArray;
 
 public class MoveAllZeroInRightSide {
-	
-	
-	public static int[] moveZero(int[] Array) {
-		
-		int index=0;
-		for(int i=0;i<Array.length;i++) {
-			
-			if(Array[i]!=0) {
-				Array[index++]=Array[i];
-			}
-			
-			
-		}
-		
-		while(index<Array.length) {
-			Array[index++]=0;
-		}
-		
-		return Array;
-		
-	}
+    
+    public static int[] moveZero(int[] array) {
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 0) {
+                array[index++] = array[i];
+            }
+        }
+        while (index < array.length) {
+            array[index++] = 0;
+        }
+        return array;
+    }
 
-	public static void main(String[] args) {
-		
-		
-		int[] Array={1,2,3,4,5,0,0,0,9};
-		//int[] ans=new int[Array.length];
-		
-		try {
-		 int ans[]=moveZero(Array);
-		 
-		 for(Integer i: ans) {
-		System.out.print(i+" ");		
-			
-		 }
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-	
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 0, 0, 0, 9};
+        int[] ans = null;  // ✅ Declare outside try so it's accessible after
 
-	}
+        try {
+            ans = moveZero(array);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        // ✅ Now safely print outside the try block
+        if (ans != null) {
+            for (int i : ans) {
+                System.out.print(i + " ");
+            }
+        } else {
+            System.out.println("Error occurred, no output to print.");
+        }
+    }
 }
