3fac06015ca695e48045e25290609c9906ac772d
Amit Kumar <kumaramit1996.1@gmail.com>
2025-05-31 20:34:17 +0530

    Singleton

--- src/singelton/ClassDao_04_07.java
+++ src/singelton/ClassDao_04_07.java
@@ -1,28 +1,38 @@
 package singelton;
 
 public class ClassDao_04_07 {
-	
-	private static  ClassDao_04_07 creClassDao_04_07= null;
-	
-	private static ClassDao_04_07 getInstance() {
-		
-		if(creClassDao_04_07 == null) {
-			creClassDao_04_07 = new ClassDao_04_07();
-			
-		}
-		return creClassDao_04_07;
-	}
-	
 
-	public static void main(String[] args) {
-		
-		ClassDao_04_07 obj1= new ClassDao_04_07().getInstance();
-		System.out.println(obj1.hashCode());
-		
-		ClassDao_04_07 obj2= new ClassDao_04_07().getInstance();
-		System.out.println(obj2.hashCode());
-		// TODO Auto-generated method stub
+    // Volatile ensures visibility in multi-threaded environments
+    private static volatile ClassDao_04_07 instance = null;
 
-	}
+    // Private constructor prevents external instantiation
+    private ClassDao_04_07() {
+        if (instance != null) {
+            throw new RuntimeException("Use getInstance() method to get the single instance");
+        }
+    }
 
+    // Global access method
+    public static ClassDao_04_07 getInstance() {
+        if (instance == null) {
+            synchronized (ClassDao_04_07.class) {
+                if (instance == null) {
+                    instance = new ClassDao_04_07();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void show() {
+        System.out.println("Hello from Singleton. HashCode: " + this.hashCode());
+    }
+
+    public static void main(String[] args) {
+        ClassDao_04_07 obj1 = ClassDao_04_07.getInstance();
+        obj1.show();
+
+        ClassDao_04_07 obj2 = ClassDao_04_07.getInstance();
+        obj2.show();
+    }
 }
