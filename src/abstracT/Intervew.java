c8c4d792eb4627b838d8eace4c2566dd74354fb6
Amit Kumar <kumaramit1996.1@gmail.com>
2025-06-08 16:16:09 +0530

    String reverse

--- src/abstracT/Intervew.java
+++ src/abstracT/Intervew.java
@@ -6,7 +6,7 @@ import java.util.stream.Stream;
 
 public class Intervew {
 
-	public String reverse(String str) {
+	public static String  reverse(String str) {
 		
 		String newString="";
 		for(int i=str.length()-1;i>=0;i--) {
@@ -23,17 +23,26 @@ public class Intervew {
 		
 		String s="amit is a good boy";
 		
+		String str="";
+		
 		// tima si a doog yob
 		
+		String[] A=s.split(" ");
 		
+//		for(String i:A)
+//		
+//		System.out.println(i);
 		
-		for(int i=0;i<s.length();i++) {
+		for(int i=0;i<A.length;i++) {
 			
-			
+			str +=" "+reverse(A[i]);
 			
 			
 			
 		}
+		
+		str.trim();
+		System.out.println(str.toString());
 	
     
 	
