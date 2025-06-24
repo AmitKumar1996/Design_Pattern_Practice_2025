22430c2ad8fd98e2fff7216bb41f7402fd8e8cf8
Amit Kumar <kumaramit1996.1@gmail.com>
2025-06-24 23:50:32 +0530

    start with

--- src/Java_8/StreemAPI/ConcatenateStrings.java
+++ src/Java_8/StreemAPI/ConcatenateStrings.java
@@ -27,5 +27,13 @@ public class ConcatenateStrings {
     	
     	Optional<Integer> first = asList2.stream().distinct().sorted(Comparator.reverseOrder()).skip(1).findFirst();
     	System.out.println(first.get());
+    	
+    	 List<String> list1 = Arrays.asList("apple", "orange", "berry","kiwi","berry", "blueberry");
+         
+	        List<String> newList1 = list1.stream()
+	                .map(str -> str.toUpperCase())           // Convert to uppercase
+	                .filter(i -> i.startsWith("B"))     // Filter strings that start with "B"
+	                .collect(Collectors.toList());      // Collect to a list            
+      System.out.println("2--->"+newList1);
     }
 }
