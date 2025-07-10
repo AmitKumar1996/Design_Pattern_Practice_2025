d0e759067226e0a5d7001b41b0aa24364da99d3f
Amit Kumar <kumaramit1996.1@gmail.com>
2025-07-10 23:15:12 +0530

    Employees

--- src/abstracT/singeltonPWC.java
+++ src/abstracT/singeltonPWC.java
@@ -101,6 +101,24 @@ public class singeltonPWC {
         quickSort(arr, 0, arr.length - 1);
         System.out.println("After Quick Sort: " + Arrays.toString(arr));
     }
+    
+    static int maxProfit(int[] prices) {
+        int n = prices.length;
+        int res = 0;
+
+        // Explore all possible ways to buy and sell stock
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                res = Math.max(res, prices[j] - prices[i]);
+            }
+        }
+        return res;
+    }
+
+    public static void main1(String[] args) {
+        int[] prices = {7, 10, 1, 3, 6, 9, 2};
+        System.out.println(maxProfit(prices));
+    }
 
     // --- Quick Sort Implementation ---
     public static void quickSort(int[] arr, int low, int high) {
--- src/singelton/SingletonWithReflixive.java
+++ src/singelton/SingletonWithReflixive.java
@@ -1,23 +1,24 @@
 package singelton;
 
+// Thread-safe and Reflection-safe Singleton Pattern
+
 public class SingletonWithReflixive {
 
-    // Eager initialization of instance
+    // Eager initialization
     private static final SingletonWithReflixive instance = new SingletonWithReflixive();
 
-    // Flag to prevent reflection-based creation
+    // Flag to detect second instantiation
     private static boolean instanceCreated = false;
 
     // Private constructor
     private SingletonWithReflixive() {
-        // Prevent instantiation via reflection
         if (instanceCreated) {
             throw new RuntimeException("Use getInstance() method to get the single instance of this class.");
         }
         instanceCreated = true;
     }
 
-    // Public method to return the same instance (synchronized not required for eager initialization)
+    // Public method to provide access to the instance
     public static SingletonWithReflixive getInstance() {
         return instance;
     }
@@ -26,5 +27,22 @@ public class SingletonWithReflixive {
     public void showMessage() {
         System.out.println("Hello from Singleton!");
     }
-}
 
+    // Test class to demonstrate singleton
+    public static void main(String[] args) {
+        SingletonWithReflixive s1 = SingletonWithReflixive.getInstance();
+        s1.showMessage();
+
+        // Uncommenting below code will throw exception due to reflection
+        /*
+        try {
+            Constructor<Singleton> constructor = Singleton.class.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            Singleton s2 = constructor.newInstance();
+            s2.showMessage();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
+    }
+}
