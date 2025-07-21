950320af949b8020fe602d511a72c030c11a1f9d
Amit Kumar <kumaramit1996.1@gmail.com>
2025-07-21 23:48:45 +0530

    CrateDao_01_13

--- src/singelton/CrateDao_01_13.java
+++ src/singelton/CrateDao_01_13.java
@@ -21,15 +21,7 @@ public class CrateDao_01_13 {
 			
 		}
 		
-		synchronized (crateDao_01_13) {
-			
-			if(crateDao_01_13 == null) {
-				  crateDao_01_13= new  CrateDao_01_13();
-					
-				}
-				return crateDao_01_13;
-			
-		}
+	
 		
 	}
 	
