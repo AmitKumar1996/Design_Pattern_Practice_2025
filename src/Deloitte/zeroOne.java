109a622595d338530e756e0bf37f07dfe9b55c8f
Amit Kumar <kumaramit1996.1@gmail.com>
2025-07-31 07:47:47 +0530

    Reverse zero one

--- src/Deloitte/zeroOne.java
+++ src/Deloitte/zeroOne.java
@@ -7,23 +7,31 @@ public class zeroOne {
 		
 		int[] A= {1,0,0,1,0,1,1,0,0};
 		
-		int index=0;
+		int index=A.length-1;
+		
+		for(int i=A.length-1;i>=0;i--) {
+			if(A[i]!=0)
+			A[index--]=A[i];
+		}
+		while(index>=0) {
+			A[index--]=0;
+		}
+//		
+//		for(Integer i: A) {
+//			if(i!=0) {
+//				A[index++]=i;
+//			}
+//			
+//		}
+//		while(index<A.length) {
+//			A[index++]=0;
+//			
+//		}
+//		
+		
 		
 		for(Integer i: A) {
-			if(i!=0) {
-				A[index++]=i;
-			}
-			
-		}
-		while(index<A.length) {
-			A[index++]=0;
-			
-		}
-		
-		
-		
-		for(Integer i: A) {
-			System.out.println(i);
+			System.out.print(" "+i);
 		}
 		
 		
