cf25dc4806a30ccd7ac29dd33ef3824344cc65ed
Amit Kumar <kumaramit1996.1@gmail.com>
2025-08-14 10:24:45 +0530

    Client

--- src/LLD/SOLID/LSP_VOLITATION_DEMO/Client.java
+++ src/LLD/SOLID/LSP_VOLITATION_DEMO/Client.java
@@ -4,7 +4,16 @@ public class Client {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		playWithRectangle(new Rectangle());
+		playWithRectangle(new Squre());
 
+	}
+	static void playWithRectangle(Rectangle r) {
+		r.setHeight(10);
+		r.SetWidth(5);
+		int area = r.getArea();
+		System.out.println(area);
 	}
 
 }
--- src/LLD/SOLID/LSP_VOLITATION_DEMO/Squre.java
+++ src/LLD/SOLID/LSP_VOLITATION_DEMO/Squre.java
@@ -1,5 +1,14 @@
 package LLD.SOLID.LSP_VOLITATION_DEMO;
 
 public class Squre extends Rectangle{
+	
+	void setHeight(int ht) {
+		height=ht;
+		width=ht;
+	}
+	void SetWidth(int w) {
+		width=w;
+		height=w;
+	}
 
 }
