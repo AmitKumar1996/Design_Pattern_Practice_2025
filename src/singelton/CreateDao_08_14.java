8f025be6c2696b93166c098f8bea38fbe03a0a26
Amit Kumar <kumaramit1996.1@gmail.com>
2025-08-15 22:10:29 +0530

    singelton

---
+++ src/singelton/CreateDao_08_14.java
@@ -0,0 +1,31 @@
+package singelton;
+
+public class CreateDao_08_14 {
+	
+	private static  CreateDao_08_14   createDao_08_14=null;
+	
+	public static synchronized CreateDao_08_14 getInstance() {
+		
+		if(createDao_08_14 == null) {
+			 createDao_08_14= new CreateDao_08_14();
+			
+		}
+	    return  createDao_08_14;
+		
+	}
+
+	public static void main(String[] args) {
+		
+		
+		CreateDao_08_14 obj1= new CreateDao_08_14();
+		obj1.getInstance();
+		System.out.println(obj1.getInstance().hashCode());
+		CreateDao_08_14 obj2= new CreateDao_08_14();
+		System.out.println(obj2.getInstance().hashCode());
+		CreateDao_08_14 obj3= new CreateDao_08_14();
+		System.out.println(obj3.getInstance().hashCode());
+		
+
+	}
+
+}
