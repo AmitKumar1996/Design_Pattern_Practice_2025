923e1779da97c1f8e0bc580d11a61d10e0e11fa5
Amit Kumar <kumaramit1996.1@gmail.com>
2025-08-17 21:55:50 +0530

    MCQ

---
+++ src/MCQ/Test1.java
@@ -0,0 +1,18 @@
+package MCQ;
+
+class Test1
+{
+private int data;
+private Test1()
+{
+data = 5; 
+}
+}
+public class Test
+{
+public static void main(String[] args)
+{
+Test1 help = new Test1();
+System.out.println(help.data);
+}
+} 
