2afc1e2608dc4690ece00977a4fa7d6ed4220cda
Amit Kumar <kumaramit1996.1@gmail.com>
2025-08-19 22:40:36 +0530

    Create_Dao_19_08

---
+++ src/singelton/Create_Dao_19_08.java
@@ -0,0 +1,27 @@
+package singelton;
+
+public class Create_Dao_19_08 {
+	
+	private static Create_Dao_19_08 create_Dao_19_08=null;
+	
+	private static Create_Dao_19_08 getInstance() {
+		synchronized (create_Dao_19_08) {
+			if(create_Dao_19_08 == null) {
+				create_Dao_19_08 = new Create_Dao_19_08();
+			}
+			
+		}
+		
+		return create_Dao_19_08;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Create_Dao_19_08 obj1= new Create_Dao_19_08().getInstance();
+System.out.println(obj1.hashCode());
+Create_Dao_19_08 obj2= new Create_Dao_19_08().getInstance();
+System.out.println(obj2.hashCode());
+
+	}
+
+}
