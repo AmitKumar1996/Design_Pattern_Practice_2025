543f9161626ebfd765d0e2a3b294567b03228c76
Amit Kumar <kumaramit1996.1@gmail.com>
2025-08-26 21:34:19 +0530

    LibkedLIST

--- src/abstracT/LibkedLIST.java
+++ src/abstracT/LibkedLIST.java
@@ -21,7 +21,7 @@ public class LibkedLIST {
     
     
 
-    // ---------------- Problem 2 ------------------
+    // ---------------- Problem 2 ----------------------
     // Merge two sorted linked lists into one sorted list.
     // Input: list1 = [1,2,4], list2 = [1,3,4]
     // Output:- [1,1,2,3,4,4]
