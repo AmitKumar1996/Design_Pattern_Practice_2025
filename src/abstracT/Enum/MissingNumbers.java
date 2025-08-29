59bde0d560e658e495e42d4be8079593f286cf3c
Amit Kumar <kumaramit1996.1@gmail.com>
2025-08-29 21:16:59 +0530

    MissingNumbers

---
+++ src/abstracT/Enum/MissingNumbers.java
@@ -0,0 +1,27 @@
+package abstracT.Enum;
+import java.util.*;
+import java.util.stream.*;
+
+public class MissingNumbers {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(8);
+        list.add(10);
+
+        System.out.println("Original List: " + list);
+
+        int min = list.stream().min(Integer::compareTo).get();
+        int max = list.stream().max(Integer::compareTo).get();
+
+        List<Integer> missing = IntStream.rangeClosed(min, max)
+                .filter(i -> !list.contains(i))
+                .boxed()
+                .collect(Collectors.toList());
+
+        System.out.println("Missing Numbers: " + missing);
+    }
+}
