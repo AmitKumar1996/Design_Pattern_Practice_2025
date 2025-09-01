fe58893782546bfa41d25925e64991dd8e99f2fc
Amit Kumar <kumaramit1996.1@gmail.com>
2025-09-01 21:54:02 +0530

    Test

--- src/abstracT/Enum/MissingNumbers.java
+++ src/abstracT/Enum/MissingNumbers.java
@@ -21,5 +21,35 @@ public class MissingNumbers {
                 .collect(Collectors.toList());
 
         System.out.println("Missing Numbers: " + missing);
+        
+        
+        List<Integer> collect = list.stream().distinct().filter(i-> !list.contains(i)).collect(Collectors.toList());
+        
+        System.out.println(collect);
+    }
+    
+    public static void main(int a) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(8);
+        list.add(10);
+
+        System.out.println("Original List333333: " + list);
+
+        int min = list.stream().min(Integer::compareTo).get();
+        int max = list.stream().max(Integer::compareTo).get();
+
+        List<Integer> missing = IntStream.rangeClosed(min, max).filter(i -> !list.contains(i)).boxed()
+                .collect(Collectors.toList());
+
+        System.out.println("Missing Numbers: " + missing);
+        
+        
+        List<Integer> collect = list.stream().distinct().filter(i-> !list.contains(i)).collect(Collectors.toList());
+        
+        System.out.println(collect);
     }
 }
---
+++ src/abstracT/Test.java
@@ -0,0 +1,60 @@
+package abstracT;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Test {
+	
+	
+	
+	
+////	Function Signature:
+//		class RateLimiter {
+//		    public RateLimiter(int maxRequests, int windowInSeconds);
+//		    
+//		    maxRequests=10;
+//		    windowInSeconds=60sec;
+//		    1->1
+//		    
+//		    
+//		    public boolean allowRequest(String userId);
+//		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+//		ArrayList<Map<Integer, Integer>> listMap= (ArrayList<Map<Integer, Integer>>);
+		List<String> list=new ArrayList<String>();
+		
+		list.add("Apple");
+		list.add("Mango");
+		list.add("Banana");
+		a
+	list.stream().sorted((s1,s2)->{ return 
+		-s1.substring(1).compareTo(s2.substring(1));
+				
+				
+				}).forEach(i-> System.out.println(i));
+		
+	//	System.out.println(collect);
+		
+		
+		
+
+//Design and implement a RateLimiter class in Java that limits the number of requests per user within a fixed time window. Assume this will be used in a Spring Boot microservice to throttle API calls.
+//Requirements:
+//
+//Each user is allowed N requests per T seconds.
+//
+//If the user exceeds the limit, further requests should be rejected until the window resets.
+//
+//The solution should be thread-safe and performant under high concurrency.
+
+	}
+
+}
