bd7cef0cee3b4b9c6df642f8696f81d9121301da
Amit Kumar <kumaramit1996.1@gmail.com>
2025-09-04 22:59:15 +0530

    Test1

--- src/abstracT/Test1.java
+++ src/abstracT/Test1.java
@@ -1,54 +1,26 @@
 package abstracT;
 
+import java.util.*;
+
 public class Test1 {
-	
-//	
-//	public void print(int i,int j) {
-//		
-//	
-//		
-//	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		// Given a set of distinct integers, find all possible subsets (the power set). 
+    public static void main(String[] args) {
+        int[] A = {1, 2, 3};
+        List<List<Integer>> powerSet = new ArrayList<>();
 
-//Sample Input: {1, 2, 3} 
-//
-//Sample Output: Output: [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]] 
-		
-		int[] A= {1,2,3};
-		
-		
-		
-		
-		for(int i=0;i<A.length;i++) {  // 0
-			for(int j=0;j<A.length;j++) { // 0
-				int x=i;
-				int y=j;
-				
-				while(x<=y) {
-					System.out.print(" "+A[x]);
-					x++;
-				}
-				System.out.println();
-				
-				
-			}
-		}
-	
-		
-		
-		
-		
-			
-//			0 0-> 1 
-//			0 1-> 12
-//		    0 2->123
-//		    10->2
-//		    11->
+        int n = A.length;
+        int total = 1 << n; // 2^n subsets
 
-	}
+        for (int mask = 0; mask < total; mask++) {
+            List<Integer> subset = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    subset.add(A[i]);
+                }
+            }
+            powerSet.add(subset);
+        }
 
+        System.out.println(powerSet);
+    }
 }
