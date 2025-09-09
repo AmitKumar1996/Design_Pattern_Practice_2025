08ca2bbadffd8acfc557660498874ad756478750
Amit Kumar <kumaramit1996.1@gmail.com>
2025-09-09 22:58:13 +0530

    DbConnections

---
+++ src/LLD/Singelton/DbConnection2.java
@@ -0,0 +1,26 @@
+package LLD.Singelton;
+
+//class DbConnection{
+//	public int x=0;
+//	
+//	public void m1() {
+//		System.out.println("Hi");
+//	}
+//}
+
+
+public class DbConnection2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		DbConnection o1=new DbConnection();
+//		System.out.println(o1.x);
+//		DbConnection o2=new DbConnection();
+//		System.out.println(o2.x);
+//		DbConnection o3=new DbConnection();
+	//	o3.m1();
+
+	}
+
+}
---
+++ src/LLD/Singelton/DbConnection_2.java
@@ -0,0 +1,24 @@
+package LLD.Singelton;
+
+class DbConnections{
+	
+	//public char[] m1;
+
+	private void m12() {
+		System.out.println("V2");
+	}
+}
+
+public class DbConnection_2 {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+		DbConnections o1=new DbConnections();
+	//	System.out.println(o1.m1);
+	//o1.m1();
+	
+	}
+
+}
