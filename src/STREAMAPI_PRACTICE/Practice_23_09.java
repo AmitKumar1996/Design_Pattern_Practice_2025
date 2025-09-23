269361ef7e0fdfa8709aaea71dbbb5cdc6a56898
Amit Kumar <kumaramit1996.1@gmail.com>
2025-09-23 23:54:14 +0530

    Practice_23_09

---
+++ src/STREAMAPI_PRACTICE/Practice_23_09.java
@@ -0,0 +1,20 @@
+package STREAMAPI_PRACTICE;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.*;
+
+public class Practice_23_09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<Object> arrayList = new ArrayList<>();
+		
+		     arrayList.stream().sorted((s1,s2)->{
+		    	 return s1
+		     });
+
+	}
+
+}
