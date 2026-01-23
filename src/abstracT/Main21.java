c41ee0a1211e4e0c475811527b92cb1eae714dd1
Amit Kumar <kumaramit1996.1@gmail.com>
2026-01-23 22:51:28 +0530

    DSA

--- src/abstracT/Main21.java
+++ src/abstracT/Main21.java
@@ -9,8 +9,7 @@ public class Main21 {
 	public static List<String, Integer> EmpInfo(){
 		
 		 List<String> name=Arrays.asList("Amit","Sumit");
-		 List<Integer> age=Arrays.asList(29,28);
-		 	
+		 List<Integer> age=Arrays.asList(29,28);	
 		
 	}
 	
