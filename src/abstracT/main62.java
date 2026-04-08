39f0676f2cf0d6c974a938ec54ae69a3140fa2c6
Amit Kumar <kumaramit1996.1@gmail.com>
2026-04-08 22:50:08 +0530

    dsa

---
+++ src/abstracT/main62.java
@@ -0,0 +1,21 @@
+package abstracT;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class main62 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s="abcdcdd";
+		
+Map<Character , Integer> newMap= s.chars().mapToObj(c->(char)c).collect(Collectors.groupingBy( Function.identity(), LinkedHashMap :: new ,Collectors.counting()));
+
+	
+	
+	}
+
+}
