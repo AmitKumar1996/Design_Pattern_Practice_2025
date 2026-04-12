78bffa2723b166f94a4e5391a9016ae2fb78c969
Amit Kumar <kumaramit1996.1@gmail.com>
2026-04-12 07:48:57 +0530

    dsa

--- src/abstracT/main62.java
+++ src/abstracT/main62.java
@@ -10,9 +10,9 @@ public class main62 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String input="abcdcdd";
+		     String input="abcdcdd";
 		
-         Map<Character , Integer> map= input.chars().mapToObj(c->(char)c).collect(Collectors.groupingBy( Function.identity(), LinkedHashMap :: new ,Collectors.counting()));
+              <Character , Integer> map= input.chars().mapToObj(c->(char)c).collect(Collectors.groupingBy( Function.identity(), LinkedHashMap :: new ,Collectors.counting()));
 
 	
 	}
